9c8ae7a46856985a23283f0478c3b1ef7358b57e
sergejcheva090183-hub <sergejcheva090183@gmail.com>
2026-08-20 15:01:12 +0300

    02_lesson

--- lesson_2_task_4.py
+++ lesson_2_task_4.py
@@ -2,7 +2,6 @@
 def fizz_buzz(n):
     for i in range(1, n + 1):
        
-        
         if i % 15 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
@@ -14,4 +13,4 @@ def fizz_buzz(n):
 
 n = int(input("Введите число: "))
 
-fizz_buzz(n)
+fizz_buzz(n) 
